a2810490d07ca10bbd1327633c7b6c386c9f77ad
Tom Benner <tbb4n@virginia.edu>
2010-04-05 19:58:49 -0400

    Correcting the order of Animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -3,18 +3,18 @@ class Animal < PassiveRecord::Base
   
   schema :name => String, :id => Integer
   
-  create :name => "Dragon", :id => 1
-  create :name => "Snake", :id => 2
-  create :name => "Horse", :id => 3
-  create :name => "Sheep", :id => 4
-  create :name => "Monkey", :id => 5
-  create :name => "Cock", :id => 6
-  create :name => "Dog", :id => 7
-  create :name => "Boar", :id => 8
-  create :name => "Rat", :id => 9
-  create :name => "Tiger", :id => 10
+  create :name => "Hare", :id => 1
+  create :name => "Dragon", :id => 2
+  create :name => "Snake", :id => 3
+  create :name => "Horse", :id => 4
+  create :name => "Sheep", :id => 5
+  create :name => "Monkey", :id => 6
+  create :name => "Cock", :id => 7
+  create :name => "Dog", :id => 8
+  create :name => "Boar", :id => 9
+  create :name => "Rat", :id => 10
   create :name => "Ox", :id => 11
-  create :name => "Hare", :id => 12
+  create :name => "Tiger", :id => 12
   
   def to_s
     name
