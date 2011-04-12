6d7e5acef5f1823b128ef2b5f9c7f49890488642
Andres Montano <amontano@virginia.edu>
2011-04-12 15:10:32 -0600

    fixing spelling error in wylie

--- app/models/season.rb
+++ app/models/season.rb
@@ -6,7 +6,7 @@ class Season < PassiveRecord::Base
   create :id => 1, :gregorian_name => "Spring", :tibetan_name => "dpyid ka"
   create :id => 2, :gregorian_name => "Summer", :tibetan_name => "dbyar ka"
   create :id => 3, :gregorian_name => "Fall", :tibetan_name => "ston ka"
-  create :id => 4, :gregorian_name => "Winter", :tibetan_name => "dgon ka"
+  create :id => 4, :gregorian_name => "Winter", :tibetan_name => "dgun ka"
   
   def to_s
     "#{gregorian_name} / #{tibetan_name}"
